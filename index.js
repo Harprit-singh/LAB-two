0af5c5a098d5c1721ab196ef8fe65e197099d0c2
Harprit singh <55721002+Harprit-singh@users.noreply.github.com>
2020-03-10 13:45:52 +1300

    result

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 2;
+let b = 3;
+console.log(Math.pow(a, b));
+
+let c = 4;
+console.log(Math.pow(c, b));
+
+let e = 6;
+
+console.log(Math.pow(e,b));
+
+console.log(Math.pow(a,b) + Math.pow(c,b) + Math.pow(e,b));
+
